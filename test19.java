c32364ac3d782e7d9fa7adfaf55edf5f7b6aa85a
youho8445 <youho8445@naver.com>
2020-08-13 14:36:12 +0900

    step1

---
+++ test19.java
@@ -0,0 +1,20 @@
+public class Test19 {
+	public static void main(String[] args){
+		int score = 90;
+		String result = "";
+
+		if(score >=60){
+			result="pass";
+		}else{
+			result="fail";
+		}
+		
+		System.out.println(result);
+		System.out.println(result);
+		System.out.println(result);
+		System.out.println(result);
+		System.out.println(result);
+		System.out.println(result);
+	}
+
+}
